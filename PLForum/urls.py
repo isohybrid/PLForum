bbb2538655b422945de4878e12976defab265e3a
suma <ddllsuma@gmail.com>
2013-11-07 22:15:25 +0800

    update urls

--- PLForum/urls.py
+++ PLForum/urls.py
@@ -10,8 +10,6 @@ from lbforum.accountviews import profile
 
 from views import show_page, show_page2
 
-import plregistration
-
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 admin.autodiscover()
@@ -20,22 +18,23 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'PLForum.views.home', name='home'),
     # url(r'^PLForum/', include('PLForum.foo.urls')),
-    ###url(r'^accounts/register/$', 
-    ###    register,
-    ###    { 'backend': 'plregistration.PlBackends' },
-    ###    name='registration_register'),
+    url(r'^accounts/register/$', 
+        register,
+        { 'backend': 'plregistration.PlBackends' },
+        name='registration_register'),
+    ### a problem come out here.
     ###url(r'^user/(?P<user_id)\d+)/$', profile, name='user_profile'),
 
-    ###(r'^accounts/', include('registration.backends.default.urls')),
-    ###url(r'^captcha/', include('captcha.urls')),
-    ###(r'^admin/', include(admin.site.urls)),
+    (r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^captcha/', include('captcha.urls')),
+    (r'^admin/', include(admin.site.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     ###(r'^attachments/', include('attachments.urls')),
-    ###(r'^', include('lbforum.urls')),
+    ###(r'^forum/', include('lbforum.urls')),
     url(r'^forum/', include('plregistration.urls')),
 
     url(r'^page/', show_page),
--- plregistration/urls.py
+++ plregistration/urls.py
@@ -1,7 +1,26 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from registration.views import register
+from lbforum.accountviews import profile
+
 urlpatterns = patterns('',
-    (r'attachments/', include('attachments.urls')),
+    url(r'^accounts/register/$',
+        register,
+        { 'backend': 'plregistration.backends.PlBackends' },
+        name='registration_register'),
+    url(r'^user/(?P<user_id>\d+)/$', profile, name='user_profile'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+
+    (r'^attachments/', include('attachments.urls')),
+    url(r'^captcha/', include('captcha.urls')),
     (r'^', include('lbforum.urls')),
 )
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()

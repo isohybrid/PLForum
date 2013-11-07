9cd22286bc5385cc79ae4d250336c37da1502370
suma <ddllsuma@gmail.com>
2013-11-07 20:12:18 +0800

    correct the error

--- PLForum/urls.py
+++ PLForum/urls.py
@@ -18,25 +18,25 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'PLForum.views.home', name='home'),
     # url(r'^PLForum/', include('PLForum.foo.urls')),
-    url(r'^accounts/register/$', 
-        register,
-        { 'backend': 'plregistration.PlBackends' },
-        name='registration_register'),
-    url(r'^user/(?P<user_id)\d+)/$', profile, name='user_profile'),
+    ###url(r'^accounts/register/$', 
+    ###    register,
+    ###    { 'backend': 'plregistration.PlBackends' },
+    ###    name='registration_register'),
+    ###url(r'^user/(?P<user_id)\d+)/$', profile, name='user_profile'),
 
-    (r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^captcha/', include('captcha.urls')),
-    (r'^admin/', include(admin.site.urls)),
+    ###(r'^accounts/', include('registration.backends.default.urls')),
+    ###url(r'^captcha/', include('captcha.urls')),
+    ###(r'^admin/', include(admin.site.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     (r'^attachments/', include('attachments.urls')),
-    (r'^', include('lbforum.urls')),
+    (r'^forum/', include('lbforum.urls')),
 
     url(r'^page/', show_page),
-    url(r'^page2/', show_page2),
+    url(r'^', show_page2),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

e6f7d4b3c70c3a49e67d3bc29925304a05e09ee5
suma <ddllsuma@gmail.com>
2013-11-25 23:16:35 +0800

    correct

--- PLForum/urls.py
+++ PLForum/urls.py
@@ -20,13 +20,13 @@ urlpatterns = patterns('',
     ##url(r'^PLForum/', include('PLForum.foo.urls')),
     url(r'^accounts/register/$', 
         register,
-        { 'backend': 'plregistration.backends.PlBackends' },
+        { 'backend': 'plregistration.backends.simple.PlBackends' },
         name='registration_register'),
     # a problem come out here.
     url(r'^user/(?P<user_id)\d+)/$', profile, name='user_profile'),
 
     (r'^accounts/', include('registration.backends.default.urls')),
-    # url(r'^captcha/', include('captcha.urls')),
+    url(r'^captcha/', include('captcha.urls')),
     # (r'^admin/', include(admin.site.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- plregistration/forms.py
+++ plregistration/forms.py
@@ -2,11 +2,11 @@
 Forms and validation code for user registration.
 
 """
-from django import Forms
+from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 from registration.forms import RegistrationFormUniqueEmail
-from captcha.fields import ReCaptchaField
+from captcha.fields import CaptchaField
 
 
 
@@ -18,4 +18,4 @@ class RegistrationEmail(RegistrationFormUniqueEmail):
                               widget=forms.TextInput(attrs=attrs_dict),
                               label=_("Username"),
                               error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
-  captcha = ReCaptchaField()
+  captcha = CaptchaField()
